8994a10f184f4736f149a1a2e97f8c115929b38d
YannickDC <ynk@webdisk.be>
2019-05-22 15:00:17 +0200

    PHP Version of the same file for anyone who likes to use PHP

---
+++ getChampionNameByID.php
@@ -0,0 +1,434 @@
+<?php
+
+function getChampionIDToname($id)
+{
+    switch ($id) {
+        case 1:
+            return "Annie";
+            break;
+        case 2:
+            return "Olaf";
+            break;
+        case 3:
+            return "Galio";
+            break;
+        case 4:
+            return "TwistedFate";
+            break;
+        case 5:
+            return "XinZhao";
+            break;
+        case 6:
+            return "Urgot";
+            break;
+        case 7:
+            return "LeBlanc";
+            break;
+        case 8:
+            return "Vladimir";
+            break;
+        case 9:
+            return "Fiddlesticks";
+            break;
+        case 10:
+            return "Kayle";
+            break;
+        case 11:
+            return "Master Yi";
+            break;
+        case 12:
+            return "Alistar";
+            break;
+        case 13:
+            return "Ryze";
+            break;
+        case 14:
+            return "Sion";
+            break;
+        case 15:
+            return "Sivir";
+            break;
+        case 16:
+            return "Soraka";
+            break;
+        case 17:
+            return "Teemo";
+            break;
+        case 18:
+            return "Tristana";
+            break;
+        case 19:
+            return "Warwick";
+            break;
+        case 20:
+            return "Nunu";
+            break;
+        case 21:
+            return "MissFortune";
+            break;
+        case 22:
+            return "Ashe";
+            break;
+        case 23:
+            return "Tryndamere";
+            break;
+        case 24:
+            return "Jax";
+            break;
+        case 25:
+            return "Morgana";
+            break;
+        case 26:
+            return "Zilean";
+            break;
+        case 27:
+            return "Singed";
+            break;
+        case 28:
+            return "Evelynn";
+            break;
+        case 29:
+            return "Twitch";
+            break;
+        case 30:
+            return "Karthus";
+            break;
+        case 31:
+            return "Cho'Gath";
+            break;
+        case 32:
+            return "Amumu";
+            break;
+        case 33:
+            return "Rammus";
+            break;
+        case 34:
+            return "Anivia";
+            break;
+        case 35:
+            return "Shaco";
+            break;
+        case 36:
+            return "Dr.Mundo";
+            break;
+        case 37:
+            return "Sona";
+            break;
+        case 38:
+            return "Kassadin";
+            break;
+        case 39:
+            return "Irelia";
+            break;
+        case 40:
+            return "Janna";
+            break;
+        case 41:
+            return "Gangplank";
+            break;
+        case 42:
+            return "Corki";
+            break;
+        case 43:
+            return "Karma";
+            break;
+        case 44:
+            return "Taric";
+            break;
+        case 45:
+            return "Veigar";
+            break;
+        case 48:
+            return "Trundle";
+            break;
+        case 50:
+            return "Swain";
+            break;
+        case 51:
+            return "Caitlyn";
+            break;
+        case 53:
+            return "Blitzcrank";
+            break;
+        case 54:
+            return "Malphite";
+            break;
+        case 55:
+            return "Katarina";
+            break;
+        case 56:
+            return "Nocturne";
+            break;
+        case 57:
+            return "Maokai";
+            break;
+        case 58:
+            return "Renekton";
+            break;
+        case 59:
+            return "JarvanIV";
+            break;
+        case 60:
+            return "Elise";
+            break;
+        case 61:
+            return "Orianna";
+            break;
+        case 62:
+            return "Wukong";
+            break;
+        case 63:
+            return "Brand";
+            break;
+        case 64:
+            return "LeeSin";
+            break;
+        case 67:
+            return "Vayne";
+            break;
+        case 68:
+            return "Rumble";
+            break;
+        case 69:
+            return "Cassiopeia";
+            break;
+        case 72:
+            return "Skarner";
+            break;
+        case 74:
+            return "Heimerdinger";
+            break;
+        case 75:
+            return "Nasus";
+            break;
+        case 76:
+            return "Nidalee";
+            break;
+        case 77:
+            return "Udyr";
+            break;
+        case 78:
+            return "Poppy";
+            break;
+        case 79:
+            return "Gragas";
+            break;
+        case 80:
+            return "Pantheon";
+            break;
+        case 81:
+            return "Ezreal";
+            break;
+        case 82:
+            return "Mordekaiser";
+            break;
+        case 83:
+            return "Yorick";
+            break;
+        case 84:
+            return "Akali";
+            break;
+        case 85:
+            return "Kennen";
+            break;
+        case 86:
+            return "Garen";
+            break;
+        case 89:
+            return "Leona";
+            break;
+        case 90:
+            return "Malzahar";
+            break;
+        case 91:
+            return "Talon";
+            break;
+        case 92:
+            return "Riven";
+            break;
+        case 96:
+            return "Kog'Maw";
+            break;
+        case 98:
+            return "Shen";
+            break;
+        case 99:
+            return "Lux";
+            break;
+        case 101:
+            return "Xerath";
+            break;
+        case 102:
+            return "Shyvana";
+            break;
+        case 103:
+            return "Ahri";
+            break;
+        case 104:
+            return "Graves";
+            break;
+        case 105:
+            return "Fizz";
+            break;
+        case 106:
+            return "Volibear";
+            break;
+        case 107:
+            return "Rengar";
+            break;
+        case 110:
+            return "Varus";
+            break;
+        case 111:
+            return "Nautilus";
+            break;
+        case 112:
+            return "Viktor";
+            break;
+        case 113:
+            return "Sejuani";
+            break;
+        case 114:
+            return "Fiora";
+            break;
+        case 115:
+            return "Ziggs";
+            break;
+        case 117:
+            return "Lulu";
+            break;
+        case 119:
+            return "Draven";
+            break;
+        case 120:
+            return "Hecarim";
+            break;
+        case 121:
+            return "Kha'Zix";
+            break;
+        case 122:
+            return "Darius";
+            break;
+        case 126:
+            return "Jayce";
+            break;
+        case 127:
+            return "Lissandra";
+            break;
+        case 131:
+            return "Diana";
+            break;
+        case 133:
+            return "Quinn";
+            break;
+        case 134:
+            return "Syndra";
+            break;
+        case 136:
+            return "AurelionSol";
+            break;
+        case 141:
+            return "Kayn";
+            break;
+        case 142:
+            return "Zoe";
+            break;
+        case 143:
+            return "Zyra";
+            break;
+        case 145:
+            return "Kai'sa";
+            break;
+        case 150:
+            return "Gnar";
+            break;
+        case 154:
+            return "Zac";
+            break;
+        case 157:
+            return "Yasuo";
+            break;
+        case 161:
+            return "Vel'Koz";
+            break;
+        case 163:
+            return "Taliyah";
+            break;
+        case 164:
+            return "Camille";
+            break;
+        case 201:
+            return "Braum";
+            break;
+        case 202:
+            return "Jhin";
+            break;
+        case 203:
+            return "Kindred";
+            break;
+        case 222:
+            return "Jinx";
+            break;
+        case 223:
+            return "TahmKench";
+            break;
+        case 236:
+            return "Lucian";
+            break;
+        case 238:
+            return "Zed";
+            break;
+        case 240:
+            return "Kled";
+            break;
+        case 245:
+            return "Ekko";
+            break;
+        case 254:
+            return "Vi";
+            break;
+        case 266:
+            return "Aatrox";
+            break;
+        case 267:
+            return "Nami";
+            break;
+        case 268:
+            return "Azir";
+            break;
+        case 350:
+            return "Yuumi";
+            break;
+        case 412:
+            return "Thresh";
+            break;
+        case 420:
+            return "Illaoi";
+            break;
+        case 421:
+            return "Rek'Sai";
+            break;
+        case 427:
+            return "Ivern";
+            break;
+        case 429:
+            return "Kalista";
+            break;
+        case 432:
+            return "Bard";
+            break;
+        case 497:
+            return "Rakan";
+            break;
+        case 498:
+            return "Xayah";
+            break;
+        case 517:
+            return "Sylas";
+            break;
+        case 555:
+            return "Pyke";
+            break;
+    }
+}
+?>
