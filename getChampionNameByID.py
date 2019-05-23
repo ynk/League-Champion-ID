15f240234ba594a0f76c0c9bff874ca70c53c0f2
Yannick <40766244+YannickDC@users.noreply.github.com>
2019-05-23 23:55:55 +0200

    Adding Ornn

--- getChampionNameByID.py
+++ getChampionNameByID.py
@@ -145,6 +145,7 @@ def get_champions_name(_id):
         432: "Bard",
         497: "Rakan",
         498: "Xayah",
+        516: "Ornn",
         517: "Sylas",
         518: "Neeko",
         555: "Pyke",
